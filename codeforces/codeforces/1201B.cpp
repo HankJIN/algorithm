e2b7bbd3d59925504b7f8cffcd41b650e1c69834
YOUNGKI JIN <djrjtk@gmail.com>
2019-08-11 16:31:35 +0900

    190811

---
+++ codeforces/codeforces/1201B.cpp
@@ -0,0 +1,33 @@
+//https://codeforces.com/problemset/problem/1201/B
+//round577_B.zero Array
+#include <bits/stdc++.h>
+#define fastcin() std::ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long a
+#define rep(i,bigin,end) for(int i = (bigin); i < (end); i++)
+#define endl "\n"
+#define all(v) (v).begin(),(v).end() 
+
+using namespace std;
+bool answer;
+ll a, maxnum, sum;
+
+int main() {
+	fastcin();
+	
+	return 0;
+}
+
+/*
+틀렸다. 문제을 잘못 해석한듯하다.
+cin >> n;
+set<int>::iterator itr;
+rep(i, 0, n) {
+cin >> temp;
+if(myset.find(temp)!=myset.end())	myset.erase(myset.find(temp));
+else myset.insert(temp);
+}
+
+if (myset.size() == 0)cout << "YES";
+else cout << "NO";
+
+*/
