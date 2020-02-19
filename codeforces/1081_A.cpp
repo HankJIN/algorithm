e32a6d2bfd55e4eda3d9d686ef3e51816cde8498
YOUNGKI JIN <djrjtk@gmail.com>
2020-02-19 09:12:23 +0900

    1D1C

---
+++ codeforces/1081_A.cpp
@@ -0,0 +1,18 @@
+//https://codeforces.com/problemset/problem/1081/A
+//code by Hank Jin               blog HankJin.github.io
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int n, temp;
+
+int main(){
+    
+    std::ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    cin>> n;
+    if(n!=2)    cout <<1;
+    else cout << 2;
+}
