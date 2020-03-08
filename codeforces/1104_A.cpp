e3484c3532ed8e9b34df0e964840e54e13256bcc
YOUNGKI JIN <djrjtk@gmail.com>
2020-03-08 09:50:51 +0900

    1D1C

---
+++ codeforces/1104_A.cpp
@@ -0,0 +1,22 @@
+//https://codeforces.com/problemset/problem/1104/A
+//code by Hank Jin               blog HankJin.github.io
+
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+
+int n;
+
+int main(){
+    
+    std::ios_base::sync_with_stdio(false); cin.tie();
+    
+    cin >> n;
+
+    cout << n << endl;
+    for(int i = 0; i< n; i++){
+        cout << 1 << " ";
+    }
+}
